82c619d880847b3ea1e724f79c98790066cd40a7
James Briggs <jfbriggs106@gmail.com>
2021-04-08 10:41:12 -0700

    Add code to main, test files

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ def hello():
     return "Hello, world!"
 
 
+def goodbye():
+    return "Goodbye!"
+
+
 if __name__ == "__main__":
     print(hello())
--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import unittest
 
-from main import hello
+from main import hello, goodbye
 
 
 class BasicTest(unittest.TestCase):
 
-    def hello_test(self):
+    def test_hello(self):
         self.assertEqual(hello(), "Hello, world!")
+
+    def test_goodbye(self):
+        self.assertEqual(goodbye(), "Goodbye!")
+
+
+if __name__ == "__main__":
+    unittest.main()
